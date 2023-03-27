9320af68c2aaa206954d4ccd7ed1e7228915718c
Rahmidin <rahmiddin.hasanov@bk.ru>
2023-03-27 13:21:48 +0500

    add_models_in_admin_panel

--- Shop/backend/admin.py
+++ Shop/backend/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Shop, Street, City
 # Register your models here.
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    fields = ['name', 'city', 'street', 'building', 'opening_time', 'closing_time']
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    fields = ['name', 'city']
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    fields = ['name', ]
--- Shop/backend/models.py
+++ Shop/backend/models.py
@@ -18,11 +18,25 @@ class Street(models.Model):
     name = models.CharField(max_length=20, unique=True, verbose_name='street name')
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='street')
 
+    class Meta:
+        verbose_name = 'Street'
+        verbose_name_plural = 'Streets'
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Shop(models.Model):
     name = models.CharField(max_length=20, unique=True, verbose_name='shop name')
     city = models.ManyToManyField(City, related_name='city_shop')
     street = models.ForeignKey(Street, related_name='street_shop', on_delete=models.CASCADE)
     building = models.CharField(max_length=20, verbose_name='shop_building')
-    opening_time = models.DateTimeField()
-    closing_time = models.DateTimeField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    class Meta:
+        verbose_name = 'Shops'
+        verbose_name_plural = 'Shops'
+
+    def __str__(self):
+        return f'{self.name}'
